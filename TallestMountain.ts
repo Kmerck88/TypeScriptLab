30e625e3b6c0a21332fba34a9856c2a9e2243df1
Kevin Merck <kevin.merck88@gmail.com>
2022-10-26 13:06:25 -0400

    Create TallestMountain.ts

---
+++ TallestMountain.ts
@@ -0,0 +1,14 @@
+interface Mountain {
+name: string; 
+height: number;  
+}
+
+
+
+
+let mountains: Mountain = <string | number>[]; 
+mountains = ['Kilimanjaro', 19341, 'Everest' , 29029, 'Denali', 20310]
+
+findNameOfTallestMMountain() {
+    
+}
